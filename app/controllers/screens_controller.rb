746740e617e1606488b92f34241d5a78c243c529
natarajan <natarajan@arrivusystem.com>
2013-04-27 17:29:24 +0530

    First commit

---
+++ app/controllers/screens_controller.rb
@@ -0,0 +1,4 @@
+class ScreensController < ApplicationController
+  def home
+  end
+end
---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+OmniauthApp::Application.config.secret_token = 'f043fd38eae29a4693407520d8dbb3c9960a35bb9def355dacb753d63ee922ec7432d47fce2a7a651db656da34e226d335585e633d27b43d62f85b72bef0e87e'
